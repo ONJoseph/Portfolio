9ddb0548d79a32fde58b6cba7ae18f02d00c9899
ONJoseph <on.josephinfo@gmail.com>
2022-04-05 15:56:01 +0100

    add functionality to hamburger menu

---
+++ main.js
@@ -0,0 +1,22 @@
+const menuIconMobile = document.querySelector('.navbar__hamb');
+const menu = document.querySelector('.navbar__menu');
+const viewport = document.querySelector('body');
+const menuLinks = document.querySelectorAll('.navbar__menu__link');
+
+function isActiveClassExisted(element) {
+    return element.classList.contains('active');
+}
+
+function refreshIcon() {
+    if (isActiveClassExisted(menu)) {
+        menuIconMobile.setAttribute('src', 'images/hamburguer-close.png');
+    } else {
+        menuIconMobile.setAttribute('src', 'images/hamburguer.png');
+    }
+}
+
+function openMenu() {
+    menu.classList.toggle('active');
+    viewport.classList.add('blockover');
+    refreshIcon();
+}
