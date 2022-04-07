b3939b2a4095e2cf47bd29be377c54b8ae4a501c
abrish <97598663+abreham1222@users.noreply.github.com>
2022-04-08 02:34:55 +0300

    Add queryselector to main.js

--- src/main.js
+++ src/main.js
@@ -14,3 +14,21 @@ menuLinks.forEach((menuLink) => {
 // Event that detects when the page loads
 document.addEventListener('DOMContentLoaded', () => {
   createModal();
+});
+
+const buttonsProject = document.querySelectorAll('.works__card button');
+const closeIcon = document.querySelector('.closeModal');
+const modal = document.querySelector('.modal');
+const backgroundModal = document.querySelector('.overlay');
+const projectName = document.querySelector('.project-name');
+const projectFeature = document.querySelector('.project-feature');
+const projectBack = document.querySelector('.project-background');
+const projectYear = document.querySelector('.project-year');
+const projectImage = document.querySelector('.project-image');
+const projectDescription = document.querySelector('.project-description');
+const projectTool1 = document.querySelector('.project-tool1');
+const projectTool2 = document.querySelector('.project-tool2');
+const projectTool3 = document.querySelector('.project-tool3');
+const projectLinkLive = document.querySelector('.project-linkLive');
+const projectLinkSource = document.querySelector('.project-linkSource');
+
