5fd8ab211e8bde1baf6ac2d150d04f2754a17cee
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-04-08 22:58:16 +0100

    fix indentation

--- src/validation.js
+++ src/validation.js
@@ -18,10 +18,10 @@ submitButton.addEventListener('click', (event) => {
 
   const invalidInput = validateInputs(formInputs);
   if (invalidInput === -1) {
-    form.submit();}
+    form.submit(); }
   else {
     span = createElement('span', 'error', {}, errorMessage[invalidInput]);
-      document.querySelector('form div').append(span);
+    document.querySelector('form div').append(span);
   }
 });
 
