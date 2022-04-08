30281d4fc1807c0238b620ce972ca70a085ff473
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-04-08 22:24:01 +0100

    fix indentation

--- src/validation.js
+++ src/validation.js
@@ -3,28 +3,28 @@ const submitButton = form['user-submit'];
 let span = '';
 const [...formInputs] = [form.name, form.email, form.message];
 const [...errorMessage] = [
-    'Please enter your name',
-    'Email should be lower case in format user@mail.com',
-    'Please enter your message'
+  'Please enter your name',
+  'Email should be lower case in format user@mail.com',
+  'Please enter your message',
 ];
 
 // When user clicks Submit button, submit form if all form inputs are valid,
 // Else display custom error message
 submitButton.addEventListener('click', (event) => {
-    removeElement(form.querySelector('span'));
-    event.preventDefault(); // Prevent default form validation messages
+  removeElement(form.querySelector('span'));
+  event.preventDefault(); // Prevent default form validation messages
 
-const invalidInput = validateInputs(formInputs);
-if (invalidInput === -1) { form.submit();
-  }
-  else {
+  const invalidInput = validateInputs(formInputs);
+    if (invalidInput === -1) 
+      { form.submit();
+      }
+    else {
       span = createElement('span', 'error', {}, errorMessage[invalidInput]);
       document.querySelector('form div').append(span);
     }
 });
 
-//Remove error message when user clicks on any form input
+// Remove error message when user clicks on any form input
 addEventListeners(formInputs, 'click', () => {
   removeElement(form.querySelector('span'));
 });
-
