6558f84b11f5bcd8f4d82aa77809725fbdfdb076
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-04-08 20:22:43 +0100

    add validation.js functionality

--- src/validation.js
+++ src/validation.js
@@ -1,10 +1,25 @@
-export const email = document.querySelector('.contact__email');
-export const form = document.querySelector('#form');
-export const error = document.querySelector('.error');
+const form = document.forms[0];
+const submitButton = form['user-submit'];
+let span = '';
+const [...formInputs] = [form.name, form.email, form.message];
+const [...errorMessage] = [
+    'Please enter your name',
+    'Email should be lower case in format user@mail.com',
+    'Please enter your message'
+];
 
-export function isItUppercase(value) {
-  if (value.match(/^[a-z@.0-9-_]*$/)) {
-    return true;
-  }
-  return false;
-}
+// When user clicks Submit button, submit form if all form inputs are valid,
+// Else display custom error message
+submitButton.addEventListener('click', (event) => {
+    removeElement(form.querySelector('span'));
+    event.preventDefault(); // Prevent default form validation messages
+
+    const invalidInput = validateInputs(formInputs);
+    if (invalidInput === -1) {
+        form.submit();
+    } else {
+        span = createElement('span', 'error', {}, errorMessage[invalidInput]);
+        document.querySelector('form div').append(span);
+    }
+});
+
