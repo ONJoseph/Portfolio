a2caed7ed85fa95af048dae2b83e43a668af00b5
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-04-09 01:02:28 +0100

    add localstorage.js functionality

---
+++ src/localstorage.js
@@ -0,0 +1,20 @@
+export const name = document.querySelector('.contact__name');
+
+export function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+    // everything except Firefox
+      e.code === 22
+            // Firefox
+            || e.code === 1014
+            // test name field too, because code might not be present
+            // everything except Firefox
+            || e.name === 'QuotaExceededError'
+            
