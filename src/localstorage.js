8ec572a176b2c85736a9b00f23283198484ed352
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-04-09 01:04:16 +0100

    add firefox storage length

--- src/localstorage.js
+++ src/localstorage.js
@@ -17,4 +17,9 @@ export function storageAvailable(type) {
             // test name field too, because code might not be present
             // everything except Firefox
             || e.name === 'QuotaExceededError'
-            
+            // Firefox
+            || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+            // acknowledge QuotaExceededError only if there's something already stored
+            && (storage && storage.length !== 0);
+  }
+}
