bf6860b31d5c228a6cb90588af0ee2b1f5ec7fe7
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-04-09 19:23:30 +0100

    fix indentation

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,9 @@
 document.querySelector('#contact').addEventListener('submit', () => {
- localStorage.clear();
-  const userInfo = {
-  username: document.querySelector('#name').value,
-  email: document.querySelector('#email').value,
-  messge: document.querySelector('#message').value,
- };
-localStorage.setItem('userdata', JSON.stringify(userInfo));
+  localStorage.clear();
+    const userInfo = {
+    username: document.querySelector('#name').value,
+    email: document.querySelector('#email').value,
+    messge: document.querySelector('#message').value,
+  };
+  localStorage.setItem('userdata', JSON.stringify(userInfo));
 });
