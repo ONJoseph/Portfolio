71877a5b0b475d4b553711337aa894b20ea56f27
ON Joseph <60672480+ONJoseph@users.noreply.github.com>
2022-04-09 13:43:45 +0100

    add localStorage retrieve functionality

--- src/localstorage.js
+++ src/localstorage.js
@@ -1,36 +1,35 @@
 /* global fetchdata, */
 
-export const name = document.querySelector('.contact__name');
-
-export function storageAvailable(type) {
-  let storage;
-  try {
-    storage = window[type];
-    const x = '__storage_test__';
-    storage.setItem(x, x);
-    storage.removeItem(x);
-    return true;
-  } catch (e) {
-    return e instanceof DOMException && (
-    // everything except Firefox
-      e.code === 22
-            // Firefox
-            || e.code === 1014
-            // test name field too, because code might not be present
-            // everything except Firefox
-            || e.name === 'QuotaExceededError'
-            // Firefox
-            || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-            // acknowledge QuotaExceededError only if there's something already stored
-            && (storage && storage.length !== 0);
+contact.addEventListener("submit", (event) => {
+    contact.submit();
   }
-}
+});
+
+const theName = document.querySelector(".contact-name");
+const yourMessage = document.querySelector(".contact-message");
+
+// listen for inputs to create form objects
+
 contact.addEventListener("input", () => {
   const dataObject = {
     gotYourName: theName.value,
     gotYourMail: mail.value,
     gotYourMessage: yourMessage.value,
   };
+
+  // saving to local storage
+
+  localStorage.setItem("formData", JSON.stringify(dataObject));
+});
+
+// retrieving data from local storage
+
+const fetchdata = JSON.parse(localStorage.getItem("formData"));
+if (fetchdata) {
+  theName.value = fetchdata.gotYourName;
+  mail.value = fetchdata.gotYourMail;
+  yourMessage.value = fetchdata.gotYourMessage;
+  }
 
   // saving to local storage
 
